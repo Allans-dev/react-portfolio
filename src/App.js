1824432c70d12cfcae1d86cc0ac74bfb3bdb6b40
Allan.W.C <allan_195@hotmail.com>
2018-12-05 20:14:00 +1100

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,30 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+
+import Header from './src/Header';
+import LandingPage from './src/LandingPage';
+import Skills from './src/Skills';
+import History from './src/History';
+import Projects from '/src/Projects';
+import AboutMe from './src/AboutMe';
+import Timeline from './src/Timeline';
+import ContactUs from './src/ContactUs';
+import Footer from './src/Footer';
+
 import './App.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <Header/>
+        <LandingPage/>
+        <Skills/>
+        <History/>
+        <Projects/>
+        <AboutMe/>
+        <Timeline/>
+        <ContactUs/>
+        <Footer/>
       </div>
     );
   }
