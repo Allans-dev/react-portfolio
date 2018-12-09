08fc37eb8c894af17e0ff2a75ad0837d9e05534b
Allan.W.C <allan_195@hotmail.com>
2018-12-09 13:47:35 +1100

    added links

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,13 +1,15 @@
 import React from 'react';
+import { Link } from 'react-router-dom'
+
 import '../style/header-styles.css';
 
 const Header = () => {
   return (
     <section className='header'>
       <nav className='header-nav'>
-        <button className='exp-btn header-btn'>experience</button>
-        <button className='projects-btn header-btn'>projects</button>
-        <button className='social-btn header-btn'>social</button>
+        <Link to={`/experience`} className='exp-btn header-btn'>experience</Link>
+        <Link to={`/projects`} className='projects-btn header-btn'>projects</Link>
+        <Link to={`/social`} className='social-btn header-btn'>social</Link>
       </nav>
     </section>
   );
