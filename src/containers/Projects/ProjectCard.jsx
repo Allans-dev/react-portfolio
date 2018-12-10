94444871b28fae40a9c8f28da74e7354486ffa6f
Allan.W.C <allan_195@hotmail.com>
2018-12-10 17:18:25 +1100

    added projects with card styling

--- src/containers/Projects/ProjectCard.jsx
+++ src/containers/Projects/ProjectCard.jsx
@@ -3,14 +3,22 @@ import React from 'react';
 import Card from '@material-ui/core/Card';
 import CardActions from '@material-ui/core/CardActions';
 import CardContent from '@material-ui/core/CardContent';
+import CardMedia from '@material-ui/core/CardMedia';
 import Button from '@material-ui/core/Button';
 import Typography from '@material-ui/core/Typography';
 
+import '../../style/projects-styles.css'
+
 const ProjectCard = (props) => {
   return (
-    <Card>
+    <Card className="project-card">
       <CardContent>
-        
+        <CardMedia 
+          component="iframe"
+          src={props.iframeSrc}
+          hieght="140"
+          width="100"
+        />
         <Typography>
           A brief description.
         </Typography>
--- src/containers/Projects/index.jsx
+++ src/containers/Projects/index.jsx
@@ -1,17 +1,29 @@
-import React from 'react';
-import Card from './ProjectCard';
+import React, { Component }from 'react';
+import ProjectCard from './ProjectCard';
+import projectList from '../../projectList';
 
 import '../../style/projects-styles.css';
 
-const Projects = () => {
+class Projects extends Component { 
+  render(){
 
+    const displayCards = (() =>{
+      return projectList.map((item) => {
+        return (
+          <ProjectCard iframeSrc={item}/>
+        );
+      });
+    })();
+
+    return (
+      <section className="projects">
+        <h1>Projects</h1>
+        {displayCards}
+      </section>
+    );
+
+  }
   
-
-  return (
-    <section className="projects">
-      <Card/>
-    </section>
-  );
 }
 
 export default Projects;
