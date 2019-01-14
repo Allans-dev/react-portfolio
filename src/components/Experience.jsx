d99d148c3ac3f95cf58581ba79f4a09391a5346a
Allan.W.C <allan_195@hotmail.com>
2019-01-14 14:59:22 +1100

    updated experience page

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -26,9 +26,28 @@ class Experience extends Component {
     return (
       <section className="experience">
         <div className="table-container">
-          <div className="cell exp"></div>
-          <div className="cell extra"></div>
-          <div className="cell org"></div>
+          <div className="cell exp">
+            <h2>Experience</h2>
+            <h3>Junior Full Stack Web Developer, BirdBrain</h3>
+            <p>Developing in HTML, CSS,JavaScript, ReactJS, PHP and WordPress</p>
+          </div>
+          <div className="cell edu">
+            <h2>Education</h2>
+            <ul>
+              <li>Free Code Camp's Front End Certificate 2017</li>
+              <li>Free Code Camp's Algorthms and Data Structures Certificate</li>
+              <li>Bachelors Property Economics, University of Technology Sydney</li>
+              <li>International Business, Berlin University of Social Sciences</li>
+            </ul>
+          </div>
+          <div className="cell extra">
+            <h2>Extra-Curricular</h2>
+            <ul>
+              <li>free Code Camp - Top Contributor 2018 - hosting a study group</li>
+              <li>volunteer helper at Muses Code React Workshop</li>
+              <li>attended technology meetups in 2017</li>
+            </ul>
+          </div>
           <div className="cell fun">
             <Button type="button" onClick={this.showModal}>
               Open
