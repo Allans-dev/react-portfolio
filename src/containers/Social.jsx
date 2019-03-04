ccf3fc9342f43d55abab519c2e544df2be24391f
Allan.W.C <allan_195@hotmail.com>
2019-03-04 16:00:20 +1100

    added links

--- src/containers/Social.jsx
+++ src/containers/Social.jsx
@@ -12,10 +12,10 @@ class Social extends Component {
     return (
       <section className="social">
         <div className="icon-container">
-          <i className="fa fa-linkedin" />
-          <i className="fa fa-github" />
-          <i className="fa fa-free-code-camp" />
-          <i className="fa fa-instagram" />
+          <a href="https://www.linkedin.com/in/allan-cheung-web/" target="_blank" rel="noopener noreferrer"><i className="fa fa-linkedin" /></a>
+          <a href="https://github.com/Allans-dev" target="_blank" rel="noopener noreferrer"><i className="fa fa-github" /></a>
+          <a href="https://www.freecodecamp.org/allans-dev" target="_blank" rel="noopener noreferrer"><i className="fa fa-free-code-camp" /></a>
+          <a href="https://www.instagram.com/al_che9/" target="_blank" rel="noopener noreferrer"><i className="fa fa-instagram" /></a>
         </div>
         <div className="social-title">
             Social-Media
