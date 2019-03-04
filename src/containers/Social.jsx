05333b3414639be7c4ba00e93ba908516dc7493c
Allan.W.C <allan_195@hotmail.com>
2019-03-04 15:37:33 +1100

    added icons and text animation

--- src/containers/Social.jsx
+++ src/containers/Social.jsx
@@ -11,7 +11,15 @@ class Social extends Component {
   render() {
     return (
       <section className="social">
-        <div className="icon-container">Social^^^</div>
+        <div className="icon-container">
+          <i className="fa fa-linkedin" />
+          <i className="fa fa-github" />
+          <i className="fa fa-free-code-camp" />
+          <i className="fa fa-instagram" />
+        </div>
+        <div className="social-title">
+            Social-Media
+        </div>
       </section>
     );
   }
