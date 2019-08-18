ff168e7f8474ad2352286c94ee82f769fac00734
Allan.W.C <allan_195@hotmail.com>
2019-08-18 17:37:49 +1000

    deploy env check for router

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -5,10 +5,18 @@ import Projects from '../containers/Projects';
 import Experience from './Experience';
 import Social from '../containers/Social';
 
+function envCheck() {
+  if (!process.env.NODE_ENV || process.env.NODE_ENV === 'development') {
+        return <Route exact path="/" component={Home} />
+      } else {
+        return <Route exact path="/react-portfolio" component={Home} />
+      }
+}
+
 const Main = () => (
   <main>
     <Switch>
-      <Route exact path="/" component={Home} />
+      {envCheck()}
       <Route path="/experience" component={Experience} />
       <Route path="/projects" component={Projects} />
       <Route path="/social" component={Social} />
