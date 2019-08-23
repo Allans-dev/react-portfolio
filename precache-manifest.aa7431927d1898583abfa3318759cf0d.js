e680aacbb4a272f4740e658588f2b3c0ec78fa1f
Allan.W.C <allan_195@hotmail.com>
2019-08-23 14:40:35 +1000

    Updates

--- precache-manifest.aa7431927d1898583abfa3318759cf0d.js
+++ precache-manifest.aa7431927d1898583abfa3318759cf0d.js
@@ -1,11 +1,11 @@
 self.__precacheManifest = [
   {
-    "revision": "a953987bf9c69f781af4",
+    "revision": "4c422ad13b73186ece7e",
     "url": "/react-portfolio/static/css/main.f7366a8b.chunk.css"
   },
   {
-    "revision": "a953987bf9c69f781af4",
-    "url": "/react-portfolio/static/js/main.a953987b.chunk.js"
+    "revision": "4c422ad13b73186ece7e",
+    "url": "/react-portfolio/static/js/main.4c422ad1.chunk.js"
   },
   {
     "revision": "1e4ad457277730ac33c8",
@@ -20,27 +20,27 @@ self.__precacheManifest = [
     "url": "/react-portfolio/static/js/2.4ad32da6.chunk.js"
   },
   {
-    "revision": "af7ae505a9eed503f8b8e6982036873e",
-    "url": "/react-portfolio/static/media/fontawesome-webfont.af7ae505.woff2"
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/react-portfolio/static/media/fontawesome-webfont.674f50d2.eot"
   },
   {
     "revision": "fee66e712a8a08eef5805a46892932ad",
     "url": "/react-portfolio/static/media/fontawesome-webfont.fee66e71.woff"
   },
   {
-    "revision": "b06871f281fee6b241d60582ae9369b9",
-    "url": "/react-portfolio/static/media/fontawesome-webfont.b06871f2.ttf"
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/react-portfolio/static/media/fontawesome-webfont.af7ae505.woff2"
   },
   {
-    "revision": "674f50d287a8c48dc19ba404d20fe713",
-    "url": "/react-portfolio/static/media/fontawesome-webfont.674f50d2.eot"
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/react-portfolio/static/media/fontawesome-webfont.b06871f2.ttf"
   },
   {
     "revision": "912ec66d7572ff821749319396470bde",
     "url": "/react-portfolio/static/media/fontawesome-webfont.912ec66d.svg"
   },
   {
-    "revision": "084499512f1bdc68a212f32a412918c8",
+    "revision": "0567a467b62ce2e0e01dd55dd8b43759",
     "url": "/react-portfolio/index.html"
   }
 ];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/react-portfolio/precache-manifest.ed20beef89e75eb159fb9cf3c16bfbec.js"
+  "/react-portfolio/precache-manifest.aa7431927d1898583abfa3318759cf0d.js"
 );
 
 workbox.clientsClaim();
