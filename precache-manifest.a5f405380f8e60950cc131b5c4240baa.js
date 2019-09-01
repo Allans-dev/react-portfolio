8f33f26f931adb69c7af5f7a5a92b83401b2ac74
Allan.W.C <allan_195@hotmail.com>
2019-09-02 08:54:27 +1000

    Updates

--- precache-manifest.a5f405380f8e60950cc131b5c4240baa.js
+++ precache-manifest.a5f405380f8e60950cc131b5c4240baa.js
@@ -1,27 +1,31 @@
 self.__precacheManifest = [
   {
-    "revision": "e5e25539a4e7f59d4bf3",
-    "url": "/react-portfolio/static/css/main.f7366a8b.chunk.css"
+    "revision": "95ffcdb38e80e0a83193",
+    "url": "/react-portfolio/static/css/main.b005a767.chunk.css"
   },
   {
-    "revision": "e5e25539a4e7f59d4bf3",
-    "url": "/react-portfolio/static/js/main.e5e25539.chunk.js"
+    "revision": "95ffcdb38e80e0a83193",
+    "url": "/react-portfolio/static/js/main.95ffcdb3.chunk.js"
   },
   {
     "revision": "1e4ad457277730ac33c8",
     "url": "/react-portfolio/static/js/runtime~main.1e4ad457.js"
   },
   {
-    "revision": "2147b2b6e36df087d42c",
+    "revision": "646695ff54e6e4b36c4b",
     "url": "/react-portfolio/static/css/2.e4c4d882.chunk.css"
   },
   {
-    "revision": "2147b2b6e36df087d42c",
-    "url": "/react-portfolio/static/js/2.2147b2b6.chunk.js"
+    "revision": "646695ff54e6e4b36c4b",
+    "url": "/react-portfolio/static/js/2.646695ff.chunk.js"
   },
   {
     "revision": "af7ae505a9eed503f8b8e6982036873e",
     "url": "/react-portfolio/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/react-portfolio/static/media/fontawesome-webfont.b06871f2.ttf"
   },
   {
     "revision": "674f50d287a8c48dc19ba404d20fe713",
@@ -32,15 +36,11 @@ self.__precacheManifest = [
     "url": "/react-portfolio/static/media/fontawesome-webfont.fee66e71.woff"
   },
   {
-    "revision": "b06871f281fee6b241d60582ae9369b9",
-    "url": "/react-portfolio/static/media/fontawesome-webfont.b06871f2.ttf"
-  },
-  {
     "revision": "912ec66d7572ff821749319396470bde",
     "url": "/react-portfolio/static/media/fontawesome-webfont.912ec66d.svg"
   },
   {
-    "revision": "0afb7e2414b964c13d75fe20eefa50f0",
+    "revision": "41a06b59bf751092c18dd44a2b101cb7",
     "url": "/react-portfolio/index.html"
   }
 ];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/react-portfolio/precache-manifest.ca367bfa0b3803d12acd024146c7d95d.js"
+  "/react-portfolio/precache-manifest.a5f405380f8e60950cc131b5c4240baa.js"
 );
 
 workbox.clientsClaim();
