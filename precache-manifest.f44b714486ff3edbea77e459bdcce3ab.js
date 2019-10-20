13cb0165ac07021c5cf1fe150c34baccf2f58fef
Allan.W.C <allan_195@hotmail.com>
2019-10-20 22:51:49 +1100

    Updates

---
+++ precache-manifest.f44b714486ff3edbea77e459bdcce3ab.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "848e7da38b5e8d516de40ae140e792d5",
+    "url": "/react-portfolio/index.html"
+  },
+  {
+    "revision": "e1c644c0cdd4b283e2f0",
+    "url": "/react-portfolio/static/css/2.8330e5fd.chunk.css"
+  },
+  {
+    "revision": "e1c35201a865b7d8e3ab",
+    "url": "/react-portfolio/static/css/main.0c661d87.chunk.css"
+  },
+  {
+    "revision": "e1c644c0cdd4b283e2f0",
+    "url": "/react-portfolio/static/js/2.57c941ab.chunk.js"
+  },
+  {
+    "revision": "e1c35201a865b7d8e3ab",
+    "url": "/react-portfolio/static/js/main.0d415608.chunk.js"
+  },
+  {
+    "revision": "175a75cdfaf1231de18b",
+    "url": "/react-portfolio/static/js/runtime-main.98067e24.js"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/react-portfolio/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/react-portfolio/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/react-portfolio/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/react-portfolio/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/react-portfolio/static/media/fontawesome-webfont.fee66e71.woff"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -11,13 +11,19 @@
  * See https://goo.gl/2aRDsh
  */
 
-importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/react-portfolio/precache-manifest.ed0be26c34e2d28e2f1018aaba8eabe1.js"
+  "/react-portfolio/precache-manifest.f44b714486ff3edbea77e459bdcce3ab.js"
 );
 
-workbox.clientsClaim();
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
 
 /**
  * The workboxSW.precacheAndRoute() method efficiently caches and responds to
@@ -25,10 +31,9 @@ workbox.clientsClaim();
  * See https://goo.gl/S9QRab
  */
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
-workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/react-portfolio/index.html", {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/react-portfolio/index.html"), {
   
-  blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
