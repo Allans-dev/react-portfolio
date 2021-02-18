7e1a08b19de4730cb768fe1aaf71b8b487b0a50e
Allan <allanswebdev@gmail.com>
2021-02-18 20:18:16 +1100

    added new project site

--- src/project-data.js
+++ src/project-data.js
@@ -1,18 +1,21 @@
 const projectData = {
-  'Web Site': {
-    description: 'Website showing CSS and JavaScript including side nav and slide swiper.',
-    website: 'https://allans-dev.github.io/Practice-web-site/',
-    github: 'https://github.com/Allans-dev/responsiveTest',
+  "Web Site": {
+    description:
+      "Website showing CSS and JavaScript including side nav and slide swiper.",
+    website: "https://allans-dev.github.io/Practice-web-site/",
+    github: "https://github.com/Allans-dev/responsiveTest",
   },
   Planets: {
-    description: 'Typescript React Planetary data display, using searchbar and tabs to switch.',
-    website: 'https://allans-dev.github.io/real-universe',
-    github: 'https://github.com/Allans-dev/real-universe',
+    description:
+      "Typescript React Planetary data display, using searchbar and tabs to switch.",
+    website: "https://allans-dev.github.io/real-universe",
+    github: "https://github.com/Allans-dev/real-universe",
   },
-  'Wiki searcher': {
-    description: 'Search using Wikipedia API. Click on result to take you to the page.',
-    website: 'https://allans-dev.github.io/Wikipedia-Viewer/',
-    github: 'https://github.com/Allans-dev/Wikipedia-Viewer',
+  "Trading Budget": {
+    description:
+      "Combination of recording stock trades, calculating tax, expenses and savings",
+    website: "https://tradingbudget.web.app",
+    github: "https://github.com/awcsome/trading-budget",
   },
 };
 
