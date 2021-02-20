44c402290b92a86da28d4b6cbcf414b8e8e407ed
Allan.W.C <allanswebdev@gmail.com>
2021-02-20 13:45:52 +1100

    adding new project

---
+++ project-data.js
@@ -0,0 +1,22 @@
+const projectData = {
+  "Web Site": {
+    description:
+      "Website showing CSS and JavaScript including side nav and slide swiper.",
+    website: "https://allans-dev.github.io/Practice-web-site/",
+    github: "https://github.com/Allans-dev/responsiveTest",
+  },
+  Planets: {
+    description:
+      "Typescript React Planetary data display, using searchbar and tabs to switch.",
+    website: "https://allans-dev.github.io/real-universe",
+    github: "https://github.com/Allans-dev/real-universe",
+  },
+  "Trading Budget": {
+    description:
+      "Combination of recording stock trades, calculating tax, expenses and savings",
+    website: "https://tradingbudget.web.app",
+    github: "https://github.com/Allans-dev/trading-budget",
+  },
+};
+
+export default projectData;
